49655c2570ee47a12cd9bf1d0fa4bd2233795950
nlkl <nils.luck@outlook.com>
2017-01-11 22:25:06 +0100

    Added sandbox project

---
+++ src/Channels.Sandbox/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Channels.Sandbox
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var semaphore = new SemaphoreSlim(0, 1);
+
+
+            var ctSource = new CancellationTokenSource();
+            var ct = ctSource.Token;
+            ctSource.CancelAfter(100);
+
+            var signalled = semaphore.Wait(Timeout.Infinite, ct);
+            Console.WriteLine($"Signalled: {signalled}");
+            Console.ReadKey();
+        }
+    }
+}
