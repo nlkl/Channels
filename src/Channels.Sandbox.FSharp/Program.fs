c38df8da643ed8a0c5830ad80059ddd6c0655a9c
nlkl <nils.luck@outlook.com>
2017-01-31 23:51:00 +0100

    Playing around with API

--- src/Channels.Sandbox.FSharp/Program.fs
+++ src/Channels.Sandbox.FSharp/Program.fs
@@ -3,20 +3,35 @@
 [<EntryPoint>]
 let main argv = 
     let chan = Channel.createBuffered 10
-    let write = Channel.write chan
-    write 1
-    write 2
-    write 3
-    let read () = Channel.read chan
-    printfn "Value: %i" (read ())
-    printfn "Value: %i" (read ())
-    printfn "Value: %i" (read ())
+
+    [1 .. 10] |> List.iter (Channel.write chan)
+
+    for i in 1 .. 10 do
+        let value = Channel.read chan
+        printfn "Value: %i" value
 
     let mvar = MVar.createEmpty ()
     MVar.put mvar 1
     printfn "Value: %i" (MVar.take mvar)
     let mvar = MVar.create 2
     printfn "Value: %i" (MVar.take mvar)
+
+    let chan = Channel.createBuffered 5
+
+    let writeTask = async {
+        for i in 0 .. 100 do
+            do! Channel.asyncWrite chan i
+            printfn "Wrote: %i" i
+    }
+
+    let readTask = async {
+        for i in 0 .. 100 do
+            let! value = Channel.asyncRead chan
+            printfn "Read: %i" value
+    }
+
+    let task = Async.Parallel([writeTask; readTask])
+    Async.RunSynchronously(task) |> ignore
 
     System.Console.ReadKey() |> ignore
     0 
