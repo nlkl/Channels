fc9bd5d807a9820b7ad2a2387faea1bc401c249d
nlkl <nils.luck@outlook.com>
2017-02-05 19:39:38 +0100

    Prepared mvar cancellation test class

---
+++ src/Channels.Tests/MVar_Cancellation.cs
@@ -0,0 +1,16 @@
+﻿using Channels.Tests.Framework;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Channels.Tests
+{
+    public class MVar_Cancellation
+    {
+    }
+}
