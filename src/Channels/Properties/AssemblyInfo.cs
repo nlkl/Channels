14f2a639083039339fe40bc593d84f1adcbe142c
nlkl <nils.luck@outlook.com>
2017-02-05 23:10:36 +0100

    Enable testing of internal functionality, to allow tests of AsyncBarrier

--- src/Channels/Properties/AssemblyInfo.cs
+++ src/Channels/Properties/AssemblyInfo.cs
@@ -34,3 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Allow test project access to internal functionality
+[assembly: InternalsVisibleTo("Channels.Tests")]
